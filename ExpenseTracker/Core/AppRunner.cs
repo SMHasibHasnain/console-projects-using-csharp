e3e829e7b395534d3c2f28dc581d026c3b48f488
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-03-24 21:36:48 +0600

    Added summary of total expenses and unique categories for user review.

--- ExpenseTracker/Core/AppRunner.cs
+++ ExpenseTracker/Core/AppRunner.cs
@@ -51,6 +51,7 @@ public class AppRunner : IAppRunner
             case MenuItem.ShowTotalSummary:
                 var listLength = _expenseService.ListLength();
                 var uniqueCategoryCount = _expenseService.UniqueCategoryCount();
+                _uiHandler.ShowSummary(listLength, uniqueCategoryCount);
                 break;
 
             case MenuItem.ViewAllExpenses:
