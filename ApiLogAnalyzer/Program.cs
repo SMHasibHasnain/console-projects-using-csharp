51db273bb16bbd12731e5ae7a7778ac88fa5e4e1
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-05-04 01:07:03 +0600

    Refactor Program.cs to improve dependency injection; add ISummery and IApiLogService implementations for better structure

--- ApiLogAnalyzer/Program.cs
+++ ApiLogAnalyzer/Program.cs
@@ -1,13 +1,17 @@
 ﻿
 using ApiLogAnalyzer.Core;
 using ApiLogAnalyzer.Repo;
+using ApiLogAnalyzer.Service;
 using ApiLogAnalyzer.Shared;
 using ApiLogAnalyzer.Ui;
 
 UserSession session = new UserSession();
 Cli cli = new Cli(session);
 IApiLogRepository apiRepo = new ApiLogRepository(session);
-IAppRunner appRunner = new AppRunner(apiRepo, cli, session);
+ISummery summery = new Summery(apiRepo, session);
+IApiLogService apiLogService = new ApiLogService(session, apiRepo);
+Menu menu = new Menu(session, cli, summery);
+IAppRunner appRunner = new AppRunner(cli, session, menu, apiLogService);
 
 
 
