c4d10c890c892e1e144302eb311bf30314e961eb
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-05-03 12:19:19 +0600

    Refine ApiLogRepo by adding regex and using block

---
+++ ApiLogAnalyzer/Repo/ApiLogRepository.cs
@@ -0,0 +1,58 @@
+using System.Text.RegularExpressions;
+using ApiLogAnalyzer.Entity;
+using ApiLogAnalyzer.Shared;
+
+namespace ApiLogAnalyzer.Repo;
+
+public class ApiLogRepository : IApiLogRepository
+{
+    private readonly UserSession _session;
+    private string _filePath = Path.Combine("_data", "api_logs_sample.csv");
+    public ApiLogRepository(UserSession session)
+    {
+        _session = session;
+    }
+
+    public void Load()
+    {
+
+        if(!File.Exists(_filePath))
+        {
+            Console.WriteLine($"File not found: {_filePath}");
+            return;
+        }
+
+        using(var _reader = new StreamReader(_filePath))
+        {
+            while (!_reader.EndOfStream)
+            {
+                string line = _reader.ReadLine();
+                string pattern = @",(?=(?:[^""]*""[^""]*"")*[^""]*$)";
+                var row = Regex.Split(line, pattern);
+                
+                for(int i=0; i < row.Length; i++)
+                {
+                    row[i] = row[i].Trim();
+                }
+                
+                var newApi = new ApiLog
+                {
+                    Timestamp = row[0],
+                    HttpMethod = Enum.Parse<EHttpMethod>(row[1], true),
+                    Endpoint = row[2],
+                    HttpStatusCode = Enum.Parse<HttpStatus>(row[3], true),
+                    ResponseTimeMs = int.Parse(row[4]),
+                    ClientIp = row[5],
+                    UserAgent = row[6],
+                    RequestSizeByte = int.Parse(row[7]),
+                    ResponseSizeByte = int.Parse(row[8]),
+                    RequestId = row[9] 
+                };
+
+                _session.ApiLogDataList.Add(newApi);
+            }
+        }
+
+
+    }
+}
