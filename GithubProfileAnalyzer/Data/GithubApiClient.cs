3ca8686f1908daef57f0acb67c8cb8c30223eaf1
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-05-12 01:03:03 +0600

    Enhance GithubApiClient to accept HttpClient and deserialize user data from API response

--- GithubProfileAnalyzer/Data/GithubApiClient.cs
+++ GithubProfileAnalyzer/Data/GithubApiClient.cs
@@ -1,8 +1,27 @@
+using System.Text.Json;
+using GithubProfileAnalyzer.Model;
 using GithubProfileAnalyzer.Shared;
 
 namespace GithubProfileAnalyzer.Data;
 
-public class GithubApiClient(ICache cache) : IGithubApiClient
+public class GithubApiClient(HttpClient client, ICache cache) : IGithubApiClient
 {
     private readonly ICache _cache = cache;
+    private readonly HttpClient _client = client;
+    private readonly string _baseUrl = @"https://api.github.com/";
+
+    public async Task<User> FetchAsync(string name, string url)
+    {
+        _client.BaseAddress = new Uri(_baseUrl);
+        _client.DefaultRequestHeaders.Add("User-Agent", "GithubProfileAnalyzer");
+        _client.DefaultRequestHeaders.Add("Accept", "application/vnd.github+json");
+        HttpResponseMessage response = await _client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        System.Console.WriteLine(responseBody);
+        var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+        User user = JsonSerializer.Deserialize<User>(responseBody, options);
+        return user;
+    }
 }
+
--- GithubProfileAnalyzer/Data/IGithubApiClient.cs
+++ GithubProfileAnalyzer/Data/IGithubApiClient.cs
@@ -1,5 +1,8 @@
+using GithubProfileAnalyzer.Model;
+
 namespace GithubProfileAnalyzer.Data;
 
 public interface IGithubApiClient
 {
+    Task<User> FetchAsync(string name, string url);
 }
